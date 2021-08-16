63c7202132bcb08d702bfa0fa0a75efc308bd6ee
jltamolo <jltamolo@gmail.com>
2021-08-16 09:53:10 +0300

    updates

--- src/app/event-demo/event-demo.component.ts
+++ src/app/event-demo/event-demo.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+
   selector: 'app-event-demo',
   template: '<button (click) = "onShow()">Show</button>',
   styleUrls: ['./event-demo.component.css']
+
 })
+
 export class EventDemoComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
   onShow(){
     alert('Show button clicked!');
-  }
+   }
 }
